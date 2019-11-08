946273dd3458f311f5e7aeea12671059dc12103a
hortenssiaa@gmail.com <hortenssiaa@gmail.com>
2019-11-08 14:57:56 +0900

    test

---
+++ Chpter5/src/Chapter5_practice1_6/GitExample.java
@@ -0,0 +1,10 @@
+package Chapter5_practice1_6;
+
+public class GitExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("미노오빠가 깃허브알려주는중~");
+	}
+
+}
